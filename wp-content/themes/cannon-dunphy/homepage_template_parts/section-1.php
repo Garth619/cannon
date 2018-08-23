0c432af4bdc78fa633cd62d749b98ab899c88eb7
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-23 09:08:32 -0700

    push

--- wp-content/themes/cannon-dunphy/homepage_template_parts/section-1.php
+++ wp-content/themes/cannon-dunphy/homepage_template_parts/section-1.php
@@ -1,6 +1,14 @@
 <section id="section_one">
 	
-	<div class="sec_one_left"></div><!-- sec_one_left -->
+	<div class="sec_one_left">
+		
+		<div class="sec_one_left_inner">
+			
+			<span class="small_header">No personal injury case is too big or too small.</span><!-- small_header -->
+			
+		</div><!-- sec_one_left_inner -->
+		
+	</div><!-- sec_one_left -->
 	
 	<div class="sec_one_right"></div><!-- sec_one_right -->
 	

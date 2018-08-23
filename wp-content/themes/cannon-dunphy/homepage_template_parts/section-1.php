125590e713f68fb63a4bad3d17658154e7da5bc9
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-23 10:18:22 -0700

    push

--- wp-content/themes/cannon-dunphy/homepage_template_parts/section-1.php
+++ wp-content/themes/cannon-dunphy/homepage_template_parts/section-1.php
@@ -6,6 +6,22 @@
 			
 			<span class="small_header">No personal injury case is too big or too small.</span><!-- small_header -->
 			
+			<span class="large_header">Over $1 billion recovered since 1985. </span><!-- large_header -->
+			
+			<span class="sec_one_intro">We have recovered <strong>more $10 million awards</strong> than any other law firm in Wisconsin History. Proudly Continuing a <strong>100 Year Family History of Four Generations</strong> of lawyers representing people who have been wrongly injured. </span><!-- sec_one_intro -->
+			
+			<a class="consultation_button" href="">
+				
+				<span class="button_title">request your free consulation</span><!-- button_title -->
+				
+				<div class="button_circle">
+					
+					<?php echo file_get_contents("wp-content/themes/cannon-dunphy/images/arrows.svg"); ?>
+					
+				</div><!-- button_circle -->
+				
+			</a><!-- consultation_button -->
+			
 		</div><!-- sec_one_left_inner -->
 		
 	</div><!-- sec_one_left -->

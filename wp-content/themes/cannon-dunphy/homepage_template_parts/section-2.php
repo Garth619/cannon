4125a5d7af17b86cd58cb0b75e93cefbd6c87b40
Garrett Cullen <garrettwcullen@gmail.com>
2018-08-31 09:24:29 -0700

    push

---
+++ wp-content/themes/cannon-dunphy/homepage_template_parts/section-2.php
@@ -0,0 +1,57 @@
+<section id="section_two">
+	
+	<div class="sec_two_inner">
+		
+		<div class="sec_two_intro">
+			
+			<span class="large_header">Get the largest settlement</span><!-- large_header -->
+			
+			<div class="money_svg"><?php echo file_get_contents("wp-content/themes/cannon-dunphy/images/sp_icon_01.svg"); ?></div>
+			
+			<span class="small_header">we offer No Fee Unless We Win</span><!-- small_header -->
+			
+		</div><!-- sec_two_intro -->
+		
+		<div class="sec_two_slider">
+			
+			<div class="sec_two_single_slide one">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/sp_icon_02.svg"/>
+				
+				<span class="slide_header">over $1 billion dollars recovered since 1985</span><!-- slide_header -->
+				
+				<span class="slide_dot"></span><!-- slide_dot -->
+				
+				<span class="slide_dot"></span><!-- slide_dot -->
+				
+			</div><!-- sec_two_single_slide -->
+			
+			<div class="sec_two_single_slide two">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/sp_icon_03.svg"/>
+				
+				<span class="slide_header">no personal injury case is too big or too small</span><!-- slide_header -->
+				
+				<span class="slide_dot"></span><!-- slide_dot -->
+				
+				<span class="slide_dot"></span><!-- slide_dot -->
+				
+			</div><!-- sec_two_single_slide -->
+			
+			<div class="sec_two_single_slide three">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/sp_icon_04.svg"/>
+				
+				<span class="slide_header">experienced and dedicated<br/> lawyers</span><!-- slide_header -->
+				
+				<span class="slide_dot"></span><!-- slide_dot -->
+				
+				<span class="slide_dot"></span><!-- slide_dot -->
+				
+			</div><!-- sec_two_single_slide -->
+			
+		</div><!-- sec_two_slider -->
+		
+	</div><!-- sec_two_inner -->
+	
+</section><!-- section_two -->

3c6fe88b82b0b1a96f26b19aebbf87f1721d3572
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-05 10:48:27 -0700

    push

--- wp-content/themes/cannon-dunphy/footer.php
+++ wp-content/themes/cannon-dunphy/footer.php
@@ -1,4 +1,22 @@
-
+<footer>
+	
+	<div class="footer_inner">
+		
+		<span class="footer_request_title">request your free consultation</span><!-- footer_request_title -->
+		
+		<span class="get_started_title">get started today</span><!-- get_started_title -->
+		
+		<span class="required_title">* Required Field</span><!-- required_title -->
+		
+		<div class="form">
+			
+			<?php gravity_form(1, false, false, false, '', true, 34); ?>
+			
+		</div><!-- form -->
+		
+	</div><!-- footer_inner -->
+	
+</footer>
 
 <?php wp_footer();?>
 

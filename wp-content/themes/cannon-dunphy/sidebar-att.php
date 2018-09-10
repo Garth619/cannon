66158bafb7cb694bc8007f3c9e0d7f2f3d09994f
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-10 10:39:58 -0700

    push

---
+++ wp-content/themes/cannon-dunphy/sidebar-att.php
@@ -0,0 +1,9 @@
+<div class="att_sidebar_wrapper">
+	
+	<div class="att_sidebar_img_wrapper">
+		
+		<img src="<?php bloginfo('template_directory');?>/images/AP_img_WilliamCannon.jpg"/>
+		
+	</div><!-- att_sidebar_img_wrapper -->
+	
+</div><!-- att_sidebar_wrapper -->
---
+++ wp-content/themes/cannon-dunphy/template-bio.php
@@ -0,0 +1,50 @@
+<?php 
+
+/* Template Name: Att Bio*/
+
+get_header(); ?>
+
+
+<div class="internal_title_wrapper">
+	
+	<div class="internal_title_inner">
+		
+		<div class="att_spacer"></div><!-- att_spacer -->
+		
+		<div class="att_title_wrapper">
+			
+			<h1><?php the_title();?></h1>
+		
+			<span class="att_postion">Wisconsin Personal Injury Attorney</span><!-- att_postion -->
+			
+		</div><!-- att_title_wrapper -->
+		
+	</div><!-- internal_title_inner -->
+	
+</div><!-- internal_title_wrapper -->
+
+<div class="internal_main att_col">
+	
+		
+		<div class="att_sidebar">
+			
+			<?php get_sidebar('att'); ?>
+			
+		</div><!-- att_sidebar -->
+		
+		<div class="container content">
+		
+					    <p>Attorney William M. Cannon is a member of one of Wisconsin's best-known and most prominent legal families extending over four generations of lawyers, beginning with his grandfather's admission to the Wisconsin Bar. This four-generation legal family has practiced continuously in Milwaukee and other communities throughout Wisconsin for over 100 years. Mr. Cannon has won more $10 million paid verdicts and settlements for his clients than any other lawyer in Wisconsin. His cases have been featured on the CBS program "60 Minutes" and the "ABC Evening National News". He has continuously been listed in The Best Lawyers in America for Over 25 consecutive years.</p>
+
+							<p>Raymond J. Cannon. (Grandfather) Admitted to Wisconsin Bar, 1914. Partner in Milwaukee law firm of Cannon & Waldron. U.S. Congressman, Milwaukee (4 th District) 1933-39. He was the most famous trial lawyer of his generation. Some of his famous clients were World Heavyweight Boxing Champion Jack Dempsey, Shoeless Joe Jackson (Chicago Black Sox Scandal) and Wallis Simpson. Between 1914-1921, he successfully tried and won over 100 consecutive jury trials, resulting in a banquet hosted by the Milwaukee Bar Association honoring him for this achievement. The Federal Defender Services of Wisconsin has named an award after him in memory of his trial accomplishments, "The Ray Cannon Justice Award".</p>
+
+							<p>Judge Robert C. Cannon. (Father) Admitted to Wisconsin Bar, 1941. Milwaukee County Civil and Circuit Court Judge, successfully re-elected 1945-79; first Presiding Judge, Wisconsin Court of Appeals, 1979-82. Legal Advisor To Major League Baseball Players Association.</p>
+
+							<p>Kelly Gildea Cannon (great granddaughter of Raymond J. Cannon and daughter of William M. Cannon) is a graduate of Loyola University Chicago School of Law (J. D.) In Chicago, Illinois. She is licensed to practice law in Illinois and New York. She also has received her Masters of Law (LLM) in International Dispute Resolution at Kings College in London, England.</p>
+		
+		</div><!-- container -->
+		
+
+</div><!-- internal_main -->
+
+<?php get_footer(); ?>

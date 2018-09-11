41a46430999935d2ecc832783ee5f09cd601775a
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-11 08:25:29 -0700

    push

--- wp-content/themes/cannon-dunphy/sidebar-att.php
+++ wp-content/themes/cannon-dunphy/sidebar-att.php
@@ -3,74 +3,62 @@
 	
 	<div class="att_sidebar_img_wrapper">
 		
-		<img src="<?php bloginfo('template_directory');?>/images/AP_img_WilliamCannon.jpg"/>
+		<?php $attorney_image = get_field( 'attorney_image' ); ?>
+
+		<img src="<?php echo $attorney_image['url']; ?>" alt="<?php echo $attorney_image['alt']; ?>" />
 		
 	</div><!-- att_sidebar_img_wrapper -->
 	
 	<div class="att_bio_list_wrapper">
 		
-		<div class="att_bio_list">
 		
-			<span class="att_bio_list_title">professional memberships</span><!-- att_bio_list_title -->
+		<?php if(get_field('accolades')): ?>
 		
+			<?php while(has_sub_field('accolades')): ?>
+			
+			<div class="att_bio_list">
+			
+			<span class="att_bio_list_title"><?php the_sub_field( 'accolades_title' ); ?></span><!-- att_bio_list_title -->
+		 
 			<ul>
-				<li>Wisconsin Association for Justice</li>
-				<li>American Association for Justice</li>
-				<li>Chairman, DPT Litigation Committee, (ATLA) 1984-1986</li>
-				<li>Member, Board of Directors, Aquatic Injury Safety Litigation Group, (ATLA) 1987-1990</li>
-				<li>Diplomate, American Board of Professional Liability Attorneys. (Nationally Board Certified)</li>
-				<li>Board Certified as Trial Attorney by the National Board of Trial Advocacy</li>
-			</ul>
+		 
+				<?php if(get_sub_field('accolades_bullets')): ?>
+				 
+					<?php while(has_sub_field('accolades_bullets')): ?>
+				 
+						<li><?php the_sub_field( 'accolades_bullet' ); ?>
+						
+						
+						<?php if(get_sub_field('accolades_sub_bullets')): ?>
+						
+							<ul>
+						 
+							<?php while(has_sub_field('accolades_sub_bullets')): ?>
+						 
+									<li><?php the_sub_field( 'accolades_sub_bullets' ); ?></li>
+						    
+							<?php endwhile; ?>
+							
+							</ul>
+						 
+						<?php endif; ?>
+						
+						
+						</li>
+				    
+					<?php endwhile; ?>
+					
+					</ul>
+					
+			</div><!-- att_bio_list -->
+				 
+				<?php endif; ?>
+		    
+			<?php endwhile; ?>
+			
+		<?php endif; ?>
 		
-		</div><!-- att_bio_list -->
-		
-		<div class="att_bio_list">
-		
-			<span class="att_bio_list_title">Admitted</span><!-- att_bio_list_title -->
-		
-			<ul>
-				<li>1972 Licensed to Practice Law in the State of Wisconsin</li>
-				<li>1972 Licensed to Practice Law in the following Federal Courts:
-				<ul>
-					<li>Eastern District Court for the State of Wisconsin</li>
-					<li>Western District Court for the State of Wisconsin</li>
-				</ul>
-				</li>
-				<li>1990 Licensed to Practice Law in the Supreme Court of the United States</li>
-				<li>1999 Licensed to Practice Law in the Seventh Circuit Court of Appeals</li>
-			</ul>
-		
-		</div><!-- att_bio_list -->
-		
-		<div class="att_bio_list">
-		
-			<span class="att_bio_list_title">Education</span><!-- att_bio_list_title -->
-		
-			<ul>
-				<li>1972 Licensed to Practice Law in the State of Wisconsin</li>
-				<li>University of Paris, France</li>
-				<li>National University of Ireland, Ireland</li>
-				<li>Marquette University (A.B., 1969)</li>
-				<li>University of Wisconsin-Madison Law School (J.D., 1972)</li>
-			</ul>
-		
-		</div><!-- att_bio_list -->
-		
-		<div class="att_bio_list">
-		
-			<span class="att_bio_list_title">Accomplishments</span><!-- att_bio_list_title -->
-		
-			<ul>
-				<li>Multiple million dollar personal injury awards.</li>
-				<li>Listed in the June, 1990 issue of Milwaukee Magazine as the top personal injury trial attorney in Milwaukee.</li>
-				<li>Continuously Listed in The Best Lawyers in America for Over 25 Consecutive Years</li>
-				<li>Featured in Milwaukee Sentinel as one of four top attorneys in the "Million Dollar Club."</li>
-				<li>Named in "Super Lawyers" Magazine as one of the Top 10 Super Lawyers in Wisconsin</li>
-			</ul>
-		
-		</div><!-- att_bio_list -->
-		
-				
+
 	</div><!-- att_bio_list_wrapper -->
 	
 	
--- wp-content/themes/cannon-dunphy/template-bio.php
+++ wp-content/themes/cannon-dunphy/template-bio.php
@@ -1,6 +1,6 @@
 <?php 
 
-/* Template Name: Att Bio*/
+/* Template Name: Att Bio */
 
 get_header(); ?>
 
@@ -17,7 +17,7 @@ get_header(); ?>
 			
 			<h1><?php the_title();?></h1>
 		
-			<span class="att_postion">Wisconsin Personal Injury Attorney</span><!-- att_postion -->
+			<span class="att_postion"><?php the_field( 'attorney_sub_header' ); ?></span><!-- att_postion -->
 			
 		</div><!-- att_title_wrapper -->
 		
@@ -31,25 +31,19 @@ get_header(); ?>
 		
 		<div class="mobile_att_image_wrapper">
 			
-			<img src="<?php bloginfo('template_directory');?>/images/AP_img_WilliamCannon.jpg"/>
+			<?php $attorney_image = get_field( 'attorney_image' ); ?>
+
+			<img src="<?php echo $attorney_image['url']; ?>" alt="<?php echo $attorney_image['alt']; ?>" />
 			
 		</div><!-- mobile_att_image_wrapper -->
 			
 		<div class="container content">
 			
-			
-		
-					    <p>Attorney William M. Cannon is a member of one of Wisconsin's best-known and most prominent legal families extending over four generations of lawyers, beginning with his grandfather's admission to the Wisconsin Bar. This four-generation legal family has practiced continuously in Milwaukee and other communities throughout Wisconsin for over 100 years. Mr. Cannon has won more $10 million paid verdicts and settlements for his clients than any other lawyer in Wisconsin. His cases have been featured on the CBS program "60 Minutes" and the "ABC Evening National News". He has continuously been listed in The Best Lawyers in America for Over 25 consecutive years.</p>
-
-							<p>Raymond J. Cannon. (Grandfather) Admitted to Wisconsin Bar, 1914. Partner in Milwaukee law firm of Cannon & Waldron. U.S. Congressman, Milwaukee (4 th District) 1933-39. He was the most famous trial lawyer of his generation. Some of his famous clients were World Heavyweight Boxing Champion Jack Dempsey, Shoeless Joe Jackson (Chicago Black Sox Scandal) and Wallis Simpson. Between 1914-1921, he successfully tried and won over 100 consecutive jury trials, resulting in a banquet hosted by the Milwaukee Bar Association honoring him for this achievement. The Federal Defender Services of Wisconsin has named an award after him in memory of his trial accomplishments, "The Ray Cannon Justice Award".</p>
-
-							<p>Judge Robert C. Cannon. (Father) Admitted to Wisconsin Bar, 1941. Milwaukee County Civil and Circuit Court Judge, successfully re-elected 1945-79; first Presiding Judge, Wisconsin Court of Appeals, 1979-82. Legal Advisor To Major League Baseball Players Association.</p>
-
-							<p>Kelly Gildea Cannon (great granddaughter of Raymond J. Cannon and daughter of William M. Cannon) is a graduate of Loyola University Chicago School of Law (J. D.) In Chicago, Illinois. She is licensed to practice law in Illinois and New York. She also has received her Masters of Law (LLM) in International Dispute Resolution at Kings College in London, England.</p>
+					 <?php get_template_part( 'loop', 'page' ); ?>
 							
 							<div class="accolades_wrapper">
 								
-								<span class="accolades_title">Accolades</span><!-- accolades_title -->
+								<span class="accolades_title"><?php the_field( 'accolades_slider_title' ); ?></span><!-- accolades_title -->
 								
 								<div class="accolade_inner">
 								
@@ -61,36 +55,22 @@ get_header(); ?>
 									
 									<div class="accolade_slider">
 										
-										<div class="accolade_single_slide">
+										<?php if(get_field('accolades_logos')): ?>
+										 
+											<?php while(has_sub_field('accolades_logos')): ?>
 											
-											<img src="<?php bloginfo('template_directory');?>/images/content1_logo_01.png"/>
+												<div class="accolade_single_slide">
 											
-										</div><!-- accolade_single_slide -->
-										
-										<div class="accolade_single_slide">
+													<?php $accolade_logo = get_sub_field( 'accolade_logo' ); ?>
+		
+													<img src="<?php echo $accolade_logo['url']; ?>" alt="<?php echo $accolade_logo['alt']; ?>" />
 											
-											<img src="<?php bloginfo('template_directory');?>/images/content1_logo_01.png"/>
-											
-										</div><!-- accolade_single_slide -->
-										
-										<div class="accolade_single_slide">
-											
-											<img src="<?php bloginfo('template_directory');?>/images/content1_logo_01.png"/>
-											
-										</div><!-- accolade_single_slide -->
-										
-										<div class="accolade_single_slide">
-											
-											<img src="<?php bloginfo('template_directory');?>/images/content1_logo_01.png"/>
-											
-										</div><!-- accolade_single_slide -->
-										
-										<div class="accolade_single_slide">
-											
-											<img src="<?php bloginfo('template_directory');?>/images/content1_logo_01.png"/>
-											
-										</div><!-- accolade_single_slide -->
-										
+												</div><!-- accolade_single_slide -->
+												
+											<?php endwhile; ?>
+										 
+										<?php endif; ?>
+
 									</div><!-- accolade_slider -->
 									
 									<div class="acco_arrow acco_arrow_right">

013354f34607e7500b237b820348cf32a6cf3c93
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-11 14:57:01 -0700

    push

--- wp-content/themes/cannon-dunphy/404.php
+++ wp-content/themes/cannon-dunphy/404.php
@@ -1,12 +1,28 @@
 <?php get_header(); ?>
 
+
+
+<div class="internal_main">
 	
-				
-<h1 class="entry-title">Not Found</h1>
-				
-				
-<p>Apologies, but the page you requested could not be found.</p>
-				
-<?php // get_sidebar(); ?>
+	<div class="not_found">
+	
+	<span class="not_found_small_header">error code: 404</span><!-- not_found_small_header -->
+	
+	<h1>Page Not Found</h1>
+	
+	<span class="not_found_content">The page you were looking for appears to have been moved, deleted or does not exist. You could <span onclick="goBack()">go back</span> to where you were or head straight to our <a href="<?php bloginfo('url');?>">home page</a>. </span>
+	
+	<script type="text/javascript">
+
+		function goBack() {
+    	window.history.back();
+		}
+
+	</script>
+	
+	</div><!-- not_found -->
+	
+</div><!-- internal_main -->
+
 			
 <?php get_footer(); ?>

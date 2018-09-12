52e994a1ecee5ec8427918c15a053373f5cdade5
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-12 10:03:10 -0700

    push

---
+++ wp-content/themes/cannon-dunphy/template-contact.php
@@ -0,0 +1,83 @@
+<?php
+	
+	/* Template Name: Contact */
+	
+	get_header(); ?>
+
+
+
+<div class="internal_main">
+	
+	<div class="contact_us_wrapper">
+	
+		<span class="contact_small_header">Questions Or Concerns?</span><!-- contact_small_header -->
+		
+		<h1 class="contact_header">Contact Us Today</h1><!-- contact_header -->
+		
+		<div class="contact_info_wrapper">
+			
+			<div class="contact_info_box">
+				
+				<span class="contact_title">Brookfield Location</span><!-- contact_title -->
+				
+				<span class="contact_page_address">595 North Barker Road Brookfield, WI 53045</span><!-- contact_page_address -->
+				
+				<a class="contact_page_directions" href="">Directions</a><!-- contact_page_directions -->
+				
+			</div><!-- contact_info_box -->
+			
+			<div class="contact_info_box">
+				
+				<a class="contact_tel" href="tel:(855) 570-2676">(855) 570-2676</a><!-- contact_tel -->
+				
+				<span class="free_title">free consultations</span><!-- free_title -->
+				
+				
+				<div class="contact_social">
+					
+					<a class="social" href="" target="_blank">
+					
+						<?php echo file_get_contents("wp-content/themes/cannon-dunphy/images/footer_icon_05.svg"); ?>
+					
+					</a><!-- footer_icon_05 -->
+				
+					<a class="social" href="" target="_blank">
+					
+						<?php echo file_get_contents("wp-content/themes/cannon-dunphy/images/footer_icon_04.svg"); ?>
+					
+					</a><!-- footer_icon_05 -->
+				
+					<a class="social" href="" target="_blank">
+					
+						<?php echo file_get_contents("wp-content/themes/cannon-dunphy/images/footer_icon_03.svg"); ?>
+					
+					</a><!-- footer_icon_05 -->
+				
+					<a class="social" href="" target="_blank">
+					
+						<?php echo file_get_contents("wp-content/themes/cannon-dunphy/images/footer_icon_02.svg"); ?>
+					
+					</a><!-- footer_icon_05 -->
+					
+				</div><!-- contact_social -->
+				
+			</div><!-- contact_info_box -->
+			
+			<div class="contact_info_box">
+				
+				<span class="contact_title">Milwaukee Location</span><!-- contact_title -->
+				
+				<span class="contact_page_address">35 North Water Street Suite 819 Milwaukee, WI 53202</span><!-- contact_page_address -->
+				
+				<a class="contact_page_directions" href="">Directions</a><!-- contact_page_directions -->
+				
+			</div><!-- contact_info_box -->
+			
+		</div><!-- contact_info_wrapper -->
+		
+	</div><!-- contact_us_wrapper -->
+	
+</div><!-- internal_main -->
+
+			
+<?php get_footer(); ?>

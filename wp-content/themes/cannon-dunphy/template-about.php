a9db77d874ae71e33a2c01b19df883cfd3ce5a25
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-13 09:40:30 -0700

    push

--- wp-content/themes/cannon-dunphy/template-about.php
+++ wp-content/themes/cannon-dunphy/template-about.php
@@ -11,9 +11,9 @@ get_header(); ?>
 		
 		<div class="about_inner_left">
 			
-			<span class="about_small_header">Wisconsin Personal Injury Lawyers</span><!-- about_small_header -->
+			<span class="about_small_header"><?php the_field( 'about_small_header' ); ?></span><!-- about_small_header -->
 			
-			<h1>About Our Firm</h1>
+			<h1><?php the_field( 'about_large_header' ); ?></h1>
 			
 		</div><!-- about_inner_left -->
 		
@@ -26,28 +26,21 @@ get_header(); ?>
 	
 	<div class="about_content content">
 		
-		<p>Cannon & Dunphy S.C., was founded in 1985. No personal injury case is too big or too small for our lawyers. Over the past 30 years, we have recovered over $1 Billion in judgments and paid settlements. We have recovered more $10 Million judgments and paid settlements for our clients than any other Wisconsin law firm.* When you work with us, the same lawyers who achieved these results are the ones who will handle your case. This is our promise. The same level of skills and resources will be working for you, despite the size of your case whether it is a car accident, truck accident or nursing home abuse claim. To learn more about our philosophy, attorneys, and practice areas, or if you want to hear from past clients, visit our video center.</p>
-
-<h2>H2 Heading Style</h2>
-<ul>
-	<li>Excepteur sint occaecat cupidatat non proident</li>
-	<li>Sunt in culpa qui officia deserunt mollit anim</li>
-	<li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </li>
-	<li>Eerpteur sint occaecat cupidatat non proident</li>
-</ul>
-
-<h3>h3 heading style</h3>
-
-	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-		
+		<?php get_template_part( 'loop', 'page' ); ?>
 		
 	</div><!-- about_content -->
 	
 	<div class="about_sidebar">
 		
-		<img src="<?php bloginfo('template_directory');?>/images/intl_about_img.jpg"/>
+		<?php $about_image = get_field( 'about_image' ); ?>
 		
-		<span class="about_image_intro">Throughout the years, we have <span class="red">proven that our skills</span> can help our clients receive the settlements or jury verdicts that <span class="blue">they deserve.</span>
+		<?php if ( $about_image ) { ?>
+		
+		<img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" />
+		
+		<?php } ?>
+		
+		<span class="about_image_intro"><?php the_field( 'about_image_caption' ); ?></span>
 			
 			
 			

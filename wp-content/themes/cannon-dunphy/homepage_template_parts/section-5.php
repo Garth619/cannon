db535dd4dbaa07713b36ae88d1575201968455d5
Garrett Cullen <Garth619@users.noreply.github.com>
2018-09-16 21:43:43 -0700

    push

---
+++ wp-content/themes/cannon-dunphy/homepage_template_parts/section-5.php
@@ -0,0 +1,21 @@
+<section id="section_five">
+	
+	<div class="section_five_inner">
+		
+		<img class="skyline" src="<?php bloginfo('template_directory');?>/images/content2_icon.svg"/><!-- skyline -->
+		
+		<span class="sec_five_large_header">Excellent Attorneys Not Noise Makers</span><!-- sec_five_large_header -->
+		
+		<div class="meet_att_wrapper">
+			
+			<span class="meet_att_line left"></span><!-- meet_att_line -->
+			
+			<a class="meet_att_link" href="">Meet Our Attorneys</a><!-- meet_att_link -->
+			
+			<span class="meet_att_line right"></span><!-- meet_att_line -->
+			
+		</div><!-- meet_att_wrapper -->
+		
+	</div><!-- section_five_inner -->
+	
+</section><!-- section_five -->

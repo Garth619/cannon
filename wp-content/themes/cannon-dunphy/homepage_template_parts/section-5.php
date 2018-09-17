f50793875604e92b4cd54c9b1d5b5e66e2964cb5
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-17 09:24:55 -0700

    push

--- wp-content/themes/cannon-dunphy/homepage_template_parts/section-5.php
+++ wp-content/themes/cannon-dunphy/homepage_template_parts/section-5.php
@@ -14,6 +14,29 @@
 			
 			<span class="meet_att_line right"></span><!-- meet_att_line -->
 			
+			</div><!-- sec_five_content -->
+			
+			<div class="sec_five_content content">
+				
+				<h2>No personal injury case is too big or too small.</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+
+				<p>Dab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+				<h3>h3 heading style</h3>
+				
+				<ul>				
+					<li>Excepteur sint occaecat cupidatat non proident</li>
+					<li>Sunt in culpa qui officia deserunt mollit anim</li>
+					<li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </li>
+					<li>Eerpteur sint occaecat cupidatat non proident</li>
+				</ul>
+
+<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque.</p>
+			
 		</div><!-- meet_att_wrapper -->
 		
 	</div><!-- section_five_inner -->

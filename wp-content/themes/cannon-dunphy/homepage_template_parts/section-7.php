3a90f80aade9bef81c48bb505113e690fd098c6d
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-17 13:53:27 -0700

    push

---
+++ wp-content/themes/cannon-dunphy/homepage_template_parts/section-7.php
@@ -0,0 +1,121 @@
+<section id="section_seven">
+	
+	
+	<div class="sec_seven_inner">
+		
+		<div class="sec_seven_left">
+			
+			<span class="sec_seven_title"><span class="blue">since 1985</span> Attorneys Bill Cannon and Pat Dunphy have given wrongly injured individuals <span class="red">the justice</span> they deserve.</span><!-- sec_seven_title -->
+			
+			<a class="view_all_pa" href="">View All Practice Areas</a><!-- view_all_pa -->
+			
+		</div><!-- sec_seven_left -->
+		
+			<div class="sec_seven_right">
+			
+				<div class="sec_seven_slider">
+					
+					<div class="sec_seven_single_slide one">
+						
+						<a class="" href="">
+							
+							<div class="svg_wrapper">
+							
+								<img src="<?php bloginfo('template_directory');?>/images/pa_icon_01.svg"/>
+							
+							</div><!-- svg_wrapper -->
+							
+							<span class="sec_seven_pa_title">Auto Accidents</span><!-- sec_seven_pa_title -->
+							
+						</a>
+						
+					</div><!-- sec_seven_single_slide -->
+					
+					<div class="sec_seven_single_slide two">
+						
+						<a class="" href="">
+							
+							<div class="svg_wrapper">
+							
+								<img src="<?php bloginfo('template_directory');?>/images/pa_icon_02.svg"/>
+							
+							</div><!-- svg_wrapper -->
+							
+							<span class="sec_seven_pa_title">Personal Injury</span><!-- sec_seven_pa_title -->
+							
+						</a>
+						
+					</div><!-- sec_seven_single_slide -->
+					
+					<div class="sec_seven_single_slide three">
+						
+						<a class="" href="">
+							
+							<div class="svg_wrapper">
+							
+								<img src="<?php bloginfo('template_directory');?>/images/pa_icon_04.svg"/>
+							
+							</div><!-- svg_wrapper -->
+							
+							<span class="sec_seven_pa_title">Wrongful Death</span><!-- sec_seven_pa_title -->
+							
+						</a>
+						
+					</div><!-- sec_seven_single_slide -->
+					
+					<div class="sec_seven_single_slide four">
+						
+						<a class="" href="">
+							
+							<div class="svg_wrapper">
+							
+								<img src="<?php bloginfo('template_directory');?>/images/pa_icon_03.svg"/>
+							
+							</div><!-- svg_wrapper -->
+							
+							<span class="sec_seven_pa_title">Nursing Home Abuse</span><!-- sec_seven_pa_title -->
+							
+						</a>
+						
+					</div><!-- sec_seven_single_slide -->
+					
+					<div class="sec_seven_single_slide five">
+						
+						<a class="" href="">
+							
+							<div class="svg_wrapper">
+							
+								<img src="<?php bloginfo('template_directory');?>/images/pa_icon_05.svg"/>
+							
+							</div><!-- svg_wrapper -->
+							
+							<span class="sec_seven_pa_title">Motorcycle Accidents</span><!-- sec_seven_pa_title -->
+							
+						</a>
+						
+					</div><!-- sec_seven_single_slide -->
+					
+					<div class="sec_seven_single_slide six">
+						
+						<a class="" href="">
+							
+							<div class="svg_wrapper">
+							
+								<img src="<?php bloginfo('template_directory');?>/images/pa_icon_06.svg"/>
+							
+							</div><!-- svg_wrapper -->
+							
+							<span class="sec_seven_pa_title">Truck Accidents</span><!-- sec_seven_pa_title -->
+							
+						</a>
+						
+					</div><!-- sec_seven_single_slide -->
+					
+				</div><!-- sec_seven_slider -->
+			
+			</div><!-- sec_seven_right -->
+		
+	</div><!-- sec_seven_inner -->
+	
+	
+</section><!-- section_seven -->

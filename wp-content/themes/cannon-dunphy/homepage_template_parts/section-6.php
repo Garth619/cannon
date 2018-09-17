29ceab6b943bc93c88452fac0b201dee2ea21232
Garrett Cullen <garrettwcullen@gmail.com>
2018-09-17 09:53:17 -0700

    push

---
+++ wp-content/themes/cannon-dunphy/homepage_template_parts/section-6.php
@@ -0,0 +1,30 @@
+<section id="section_six">
+	
+	<div class="sec_six_inner">
+		
+		<span class="sec_six_title">Case Results</span><!-- sec_six_title -->
+		
+		<div class="sec_six_cr">
+			
+			<div class="sec_six_slider">
+				
+				<div class="sec_six_single_slider">
+					
+					<span class="sec_six_cr_amount">$68,000,000</span><!-- sec_six_cr_amount -->
+					
+					<span class="sec_six_small_header">insurance law judgement</span><!-- sec_six_small_header -->
+					
+					<span class="sec_six_cr_description">$68 million judgement against insurance company for failure to fulfill their duty to defend their insured violation of insurance policy issued to their client</span><!-- sec_six_cr_description -->
+					
+					<span class="sec_six_location">Location: milwaukee county</span><!-- sec_six_location -->
+					
+				</div><!-- sec_six_single_slider -->
+				
+				
+			</div><!-- sec_six_slider -->
+			
+		</div><!-- sec_six_cr -->
+		
+	</div><!-- sec_six_inner -->
+	
+</section><!-- section_six -->
